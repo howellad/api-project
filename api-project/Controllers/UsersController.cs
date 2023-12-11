8e8f415fbf03070b5168663500ec4984f527d8cd
howellad <adster.howell@gmail.com>
2023-12-11 15:30:53 +0000

    working on users controller

--- api-project/Controllers/UsersController.cs
+++ api-project/Controllers/UsersController.cs
@@ -9,22 +9,73 @@ public class UsersController : ControllerBase
 {
     private readonly UserContext _context;
 
-    public UsersController(UserContext context) {
+    public UsersController(UserContext context)
+    {
         _context = context;
     }
 
-    [HttpGet]
-    public async Task<ActionResult<UserDTO>> GetUser(long id){
+    [HttpGet("{id}")]
+    public async Task<ActionResult<UserDTO>> GetUser(long id)
+    {
         var user = await _context.Users.FindAsync(id);
         return ItemToDTO(user ?? new User());
     }
 
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Putuser(long id, UserDTO userDTO)
+    {
+        if (id != userDTO.UserId)
+        {
+            return BadRequest();
+        }
+
+        var user = await _context.Users.FindAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        user.Name = userDTO.Name;
+        user.DateOfBirth = userDTO.DateOfBirth;
+        user.Email = userDTO.Email;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException) when (!UserExists(id))
+        {
+            return NotFound();
+        }
+
+        return NoContent();
+    }
+
+    private bool UserExists(long id)
+    {
+        return _context.Users.Any(e => e.UserId == id);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(long id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        _context.Users.Remove(user);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
 
     private static UserDTO ItemToDTO(User user) =>
        new UserDTO
        {
            Id = user.Id,
            Name = user.Name,
-           Email = user.Email
+           Email = user.Email 
        };
 }
--- api-project/Models/User.cs
+++ api-project/Models/User.cs
@@ -2,21 +2,23 @@ using NuGet.Common;
 
 namespace Api.Models;
 
-public class User {
+public class User
+{
 
-    public long UserId {get;set;}
-    public Guid Id {get;set;}
-    public string Name {get;set;}
-    public DateTime DateOfBirth {get;set;}
-    public string Email {get;set;}
-    public DateTime Registered {get;set;} 
+    public long UserId { get; set; }
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Email { get; set; }
+    public DateTime Registered { get; set; }
 }
 
-public class UserDTO {
-    public long UserId {get;set;}
-    public Guid Id {get;set;}
-    public string Name {get;set;}
-    public DateTime DateOfBirth {get;set;}
-    public string Email {get;set;}
-    public DateTime Registered {get;set;} 
+public class UserDTO
+{
+    public long UserId { get; set; }
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Email { get; set; }
+    public DateTime Registered { get; set; }
 }
