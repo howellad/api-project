03ecbc2841dc160593fb015e1edc4fd78b627b33
howellad <adster.howell@gmail.com>
2023-12-11 17:33:52 +0000

    setting up db contexts properly

--- api-project/Program.cs
+++ api-project/Program.cs
@@ -7,8 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
-// builder.Services.AddDbContext<WorkoutContext>(opt =>
-//     opt.UseInternalServiceProvider());
+builder.Services.AddDbContext<WorkoutContext>(opt =>
+    opt.UseNpgsql(connectionString: 
+        @"Server=localhost; Port=5432;User=postgres Password=password123;"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
